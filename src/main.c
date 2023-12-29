85237fb5a1718300c11d7b01e75b3a8e56e7acb2
B-AJ-Amar <amarbouakaz91@gmail.com>
2023-12-30 00:27:06 +0100

    fix 50 %  quick sort

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+#include <conio.h>
 #include "sort.h" // my csv library
 // ! this main function is just for testing, i didnt make it yet
 
@@ -10,16 +12,79 @@
 #define MAX_LINE_SIZE 1024 // Max length of a line in a CSV file
 #define clearScreen system("cls") // Clear the screen in windows (clear in linux)
 
-
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_RESET   "\x1b[0m"
 // ? functions =====================================================================================
+// this function takes menu and return the choice of the user
+int menu(int num, ...){
+    int choice =0;
+    char get;
+    va_list args;
 
+    while (1) {
+        clearScreen;
+        va_start(args, num);
+        printf("%s\n",va_arg(args, char*));
+        for (int i = 0; i < num-1; i++)
+        {
+            if (i == choice)
+            {
+                printf("    %s-> %s%s\n",COLOR_BLUE,va_arg(args, char*),COLOR_RESET);
+            }
+            else
+            {
+                printf("       %s\n",va_arg(args, char*));
+            }
+        }
+        get = getch();
+        if (get==13){
+            return choice;
+        }
+        if (get==0 || get == 0xE0) 
+        {
+            get = getch();
+        }	
+		if (get==72) choice -= (choice>0)? 1:0 ;
+			// printf("UP");
+		else if (get==80) choice +=  (choice<num-2)? 1:0 ;
+			// printf("DOWN");
+        
+    }
+    
+}
+
+/*
+choice variable :
+
+first 2 bits : alocation method
+    0- array            xxxxx00
+    1- linked list      xxxxx01
+    2- binary tree      xxxxx10
+
+3 to 5 bits : sort algorithm 
+    0- bubble sort      xx000xx
+    1- selection sort   xx001xx
+    2- insertion sort   xx010xx
+    3- quick sort       xx011xx
+    4- merge sort       xx100xx
+bit 6 : sort direction
+    0- ascending        x0xxxxx
+    1- descending       x1xxxxx
+bit 7 : is hashed 
+    0- no               0xxxxxx
+    1- yes              1xxxxxx
+
+*/
 
 // ? Main ==========================================================================================
 int main() {
     // * Variables ------------------------------------------------------------------------------
     FILE *fp; // File pointer
     char path[MAX_PATH_LEN] ;
-
+    int choice = 0,tmpp;
     // * Part 1 : get a path from the user ------------------------------------------------------
     do
     {
@@ -40,11 +105,15 @@ int main() {
     
 
     // *Part 3 : choosing the alocation method ------------------------------------------------
-    int choice;
-    printf("Choose the alocation method:\n");
-    printf("    1- Array\n");
-    printf("    2- Linked List\n");
-    printf("    3- binary tree\n");
+    choice = menu(4,"Choose the alocation method:","Array","Linked List","binary tree"); // the first 3 bits
+    printf("choice %d",choice);
+    scanf("%d", &tmpp);
+    choice += menu(3,"do you want to hash data (1=yes/0=no): ","no","yes")<<6; // the 7th bit
+    printf("choice %d",choice);
+    scanf("%d", &tmpp);
+    // if he choose binary tree without hashing , we will not take a sort algorithm because it is already sorted
+    if (choice != 2) choice += menu(5,"choose the sort algorithm: ","Bubble Sort","selection Sort","insertion Sort","Quick Sort","Merge Sort")<<2; // the 3rd to 5th bits
+    choice += menu(3,"choose the sort direction: ","Ascending","Descending")<<5; // the 6th bit
     do
     {
         printf("Enter your choice: ");
@@ -122,12 +191,13 @@ int main() {
         RecordLinkedList* newLList = createRecordLinkedList();
         CSVToLinkedList(fp, newLList);
         printLinkedList(newLList);
-        LLSort(newLList, 0, 5);
-        printLinkedList(newLList);
-        // printf("hashing\n");
-        // RecordArray hashedLL = HashTableLL(newLList, 1);
-        // printf("printing\n");
-        // printRecordArray(&hashedLL);
+        // LLSort(newLList, 0, 3);
+        // printLinkedList(newLList);
+        printf("hashing\n");
+        RecordArray hashedLL = HashTableLL(newLList, 1);
+        printf("printing\n");
+        ArraySort(&hashedLL, 0, 3);
+        printRecordArray(&hashedLL);
         printf("Linked List\n");
     }
     else
